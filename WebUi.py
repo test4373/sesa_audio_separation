899e16c98ccd9bfac92aa2ff58ae9696d9818d27
test4373 <deneme2143y@gmail.com>
2024-12-28 12:47:39 +0300

    WebUi.py

--- WebUi.py
+++ WebUi.py
@@ -231,22 +231,36 @@ class IndentDumper(yaml.Dumper):
     def increase_indent(self, flow=False, indentless=False):
         return super(IndentDumper, self).increase_indent(flow, False)
 
+
 def tuple_constructor(loader, node):
+    # Load the sequence of values from the YAML node
     values = loader.construct_sequence(node)
+    # Return a tuple constructed from the sequence
     return tuple(values)
 
-yaml.SafeLoader.add_constructor('tag:yaml.org,2002:python/tuple', tuple_constructor)
+# Register the constructor with PyYAML
+yaml.SafeLoader.add_constructor('tag:yaml.org,2002:python/tuple',
+tuple_constructor)
 
 def conf_edit(config_path, chunk_size, overlap):
-    print("Using custom overlap/chunk_size values")
     with open(config_path, 'r') as f:
         data = yaml.load(f, Loader=yaml.SafeLoader)
 
+    # handle cases where 'use_amp' is missing from config:
     if 'use_amp' not in data.keys():
-        data['training']['use_amp'] = True
+      data['training']['use_amp'] = True
 
     data['audio']['chunk_size'] = chunk_size
     data['inference']['num_overlap'] = overlap
+
+    if data['inference']['batch_size'] == 1:
+      data['inference']['batch_size'] = 2
+
+    print("Using custom overlap and chunk_size values:")
+    print(f"overlap = {data['inference']['num_overlap']}")
+    print(f"chunk_size = {data['audio']['chunk_size']}")
+    print(f"batch_size = {data['inference']['batch_size']}")
+
 
     with open(config_path, 'w') as f:
         yaml.dump(data, f, default_flow_style=False, sort_keys=False, Dumper=IndentDumper, allow_unicode=True)
