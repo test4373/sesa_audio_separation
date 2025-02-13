6ab91a1b8048dc8aab76679ac26cd55a8bb01d09
test4373 <deneme2143y@gmail.com>
2025-02-13 15:40:41 +0300

    WebUi2.py

--- WebUi2.py
+++ WebUi2.py
@@ -1338,7 +1338,7 @@ def create_interface():
             print(f"Audio file listing error: {e}")
             return []
 
-    def auto_ensemble_process(audio_input, selected_models, chunk_size, overlap, use_tta, extract_instrumental, ensemble_type, weights, progress=gr.Progress()):
+    def auto_ensemble_process(audio_input, selected_models, chunk_size, overlap, use_tta, extract_instrumental, use_demud_phaseremix_inst, ensemble_type, weights, progress=gr.Progress()):
         try:
             # Klasörleri temizle ve oluştur
             shutil.rmtree(AUTO_ENSEMBLE_TEMP, ignore_errors=True)
@@ -1624,7 +1624,7 @@ def create_interface():
                         auto_model_select,
                         auto_chunk_size,
                         auto_overlap,
-                        auto_use_demud_phaseremix_inst,
+                        use_demud_phaseremix_inst,
                         auto_extract_instrumental,
                         auto_use_tta,
                         auto_ensemble_type,
